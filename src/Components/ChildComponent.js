c5d6d7cb8219b21511f88c8c7317c78b860a258b
Your Namekarim <you@example.comkarim>
2025-05-19 14:50:46 +0300

    karim Adel

--- src/Components/ChildComponent.js
+++ src/Components/ChildComponent.js
@@ -4,6 +4,8 @@ export default function ChildComponent(props) {
   return (
     <div>
       <button onClick={()=>props.show('child')}>Click</button>
+      <p> Karim adel thabet 
+      </p>
     </div>
   )
 }
